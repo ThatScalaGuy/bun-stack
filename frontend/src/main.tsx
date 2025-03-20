45113561eb638eeb0b50591631bf0b48b5abde60
Sven Herrmann <sven.herrmann@thatscalaguy.de>
2025-03-20 10:44:33 +0100

    remove Suspense wrapper from main component rendering

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, Suspense } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -7,9 +7,7 @@ import { BackendProvider } from './context/BackendContext.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BackendProvider>
-      <Suspense fallback={<div>Loading...</div>}>
-        <App />
-      </Suspense>
+      <App />
     </BackendProvider>
   </StrictMode>,
 )
